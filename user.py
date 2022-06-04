99b437e9e7d46dc76dcc8cf78c8555d306177749
nataliia-pysanka <pysankanataliia@gmail.com>
2022-06-04 10:43:39 +0300

    Divides modules for diffirent files, change params in editing of Record

--- user.py
+++ user.py
@@ -1,4 +1,15 @@
+"""
+Module describes class User with next parameters: name, surname,
+adressbook
+"""
+from address_book import AddressBook
+
+
 class User:
-    def __init__(self, name, surname):
+    """
+    Class for instance of user
+    """
+    def __init__(self, name, surname, adress_book: AddressBook):
         self.name = name
         self.surname = surname
+        self.adress_book = adress_book
